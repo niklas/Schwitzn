eec3413e13f7c14c78f06fcdfac7478977a552a7
Niklas Hofer <niklas+dev@lanpartei.de>
2025-08-25 14:12:04 +0200

    consider duration in graph

--- lib/has_duration.rb
+++ lib/has_duration.rb
@@ -8,6 +8,18 @@ module HasDuration
   def initialize(*_, **a)
     super
     durations = Array(a[:notes] || []).grep(Duration)
-    @duration = durations.first || Duration.wrap(a[:duration])
+    @duration = durations.first || Duration.wrap(a[:duration] || 0)
+  end
+
+  def size_of_set(set)
+    super(set) * @duration.total
+  end
+
+  def total_size
+    super * @weight.total
+  end
+
+  def hover_text_of_set(_)
+    "#{@duration.inspect} #{super}"
   end
 end
